5955d41c206c5536f6e3997973ed970c8296441d
Ajay@1621 <perabathulaajay222@gmail.com>
2023-06-05 04:07:26 +0000

    guide me in finding error in code

---
+++ src/components/BannerCardItem/index.js
@@ -0,0 +1,20 @@
+// Write your code here.
+
+import './index.css'
+const BannerList=(props)=>{
+        const {sourceItem}=props;
+        const {headerText,description,className}=sourceItem;
+        return (
+                <li className={className}>
+                    <div>
+                        <h1 className="heading">{headerText}</h1>
+                        <p className="description">{description}</p>
+                        <button  type="button" className="button">Show More</button>
+                    </div>
+                    
+                    
+                </li>
+        )
+}
+
+export default BannerList;
